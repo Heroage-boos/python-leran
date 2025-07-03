4a978be68907667e3aa228296015da8b312c811d
YannisYao <1958733674@qq.com>
2025-07-03 15:37:44 +0800

    Delete 第五章 directory

--- 第五章/5-6-九九乘法表.py
+++
@@ -1,7 +0,0 @@
-
-
-for index in range(9):
-    print('\n')
-    for index2 in range(index+1):
-        print("%d*%d=%d"%(index2+1,index+1,(index+1)*(index2+1)),end=" ")
-        # print('%d * %d=%d'%(index,index2,index*index2),end=' ')
